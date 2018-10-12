5da029fd61fa053eff40fd15d202c25e52b7ce5d
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-12 14:38:21 +0100

    Created a journey class

--- Oystercard/lib/journey.rb
+++ Oystercard/lib/journey.rb
@@ -1,6 +1,16 @@
-
 class Journey
+  attr_reader :entry_station, :exit_station
 
-  
+  def initialize(station)
+    @entry_station = station
+    @exit_station = nil
+  end
 
+  def end(station)
+    @exit_station = station
+  end
+
+  def fare
+    Oystercard::MIN_AMOUNT
+  end
 end
--- Oystercard/spec/journey_spec.rb
+++ Oystercard/spec/journey_spec.rb
@@ -1,5 +1,25 @@
 require 'journey'
 
 describe Journey do
+  let(:station) { double :station }
+  let(:station_2) { double :station_2 }
 
+  it "should initialize with an entry station" do
+    subject = Journey.new(station)
+    expect(subject.entry_station).to eq station
+  end
+
+  it "should have an exit station" do
+    subject = Journey.new(station)
+    subject.end(station_2)
+    expect(subject.exit_station).to eq station_2
+  end
+
+  describe "#fare" do
+    it "should return the minimum fare" do
+      subject = Journey.new(station)
+      subject.end(station_2)
+      expect(subject.fare).to eq Oystercard::MIN_AMOUNT
+    end
+  end
 end
